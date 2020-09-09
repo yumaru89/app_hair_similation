0b9f2536f272f7f2c45ca41266de7e7d72eef4c6
喜連祐大 <kizure0809@gmail.com>
2020-09-09 17:28:11 +0900

    Initial Commit

---
+++ app_hair_similation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app_hair_similation
+//
+//  Created by 喜連祐大 on 2020/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
